eccd91d1ace8a74b702e283ba17f83cd49d77959
lucasrcbranco <lucasrcbranco@gmail.com>
2024-04-13 18:54:35 -0300

    Implemeting Query to Get All Products by Category discriminator

---
+++ src/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+public sealed record GetProductsByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductsByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsByCategoryQuery(category));
+            var response = result.Adapt<GetProductsByCategoryResponse>();
+
+            return Results.Ok(response);
+        })
+        .Produces<GetProductsByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithName("GetProductsByCategory")
+        .WithDescription("Get all products by a category discriminator")
+        .WithSummary("Get all products by a category discriminator");
+    }
+}
---
+++ src/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+public sealed record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+public sealed record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsByCategoryHandler.Handle called with {@Query}", query);
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Categories.Any(c => c.Equals(query.Category, StringComparison.InvariantCultureIgnoreCase)))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductsByCategoryResult(products);
+    }
+}
