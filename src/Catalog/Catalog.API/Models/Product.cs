60db5211465e59ded6fa2bd85b50dd6cef37f59f
lucasrcbranco <lucasrcbranco@gmail.com>
2024-04-13 18:39:15 -0300

    Implemeting Query to Get all products

--- src/Catalog/Catalog.API/Models/Product.cs
+++ src/Catalog/Catalog.API/Models/Product.cs
@@ -2,6 +2,11 @@
 
 public class Product
 {
+    public Product()
+    {
+
+    }
+
     public Product(
         string name,
         string description,
@@ -16,11 +21,11 @@ public class Product
         Categories = categories;
     }
 
-    public Guid Id { get; private set; }
-    public string Name { get; private set; } = default!;
-    public string Description { get; private set; } = default!;
-    public string ImageFile { get; private set; } = default!;
-    public decimal Price { get; private set; } = default!;
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; } = default!;
 
-    public List<string> Categories { get; private set; } = new();
+    public List<string> Categories { get; set; } = new();
 }
---
+++ src/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+public sealed record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithName("GetProducts")
+        .WithDescription("Get all products")
+        .WithSummary("Get all products");
+    }
+}
