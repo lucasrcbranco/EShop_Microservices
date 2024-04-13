354e97b651fe2e4bce51b07433db6eec86eab7da
lucasrcbranco <lucasrcbranco@gmail.com>
2024-04-13 18:46:46 -0300

    Implemeting Query to Get Product by Id

---
+++ src/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public sealed record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithName("GetProductById")
+        .WithDescription("Get a Product by Id")
+        .WithSummary("Get a Product by Id");
+    }
+}
