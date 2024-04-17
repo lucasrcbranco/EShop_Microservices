a553433b2874f6d71000875603dc53bf27fcb32a
lucasrcbranco <lucasrcbranco@gmail.com>
2024-04-17 12:14:50 -0300

    Adding custom exceptions support and global error validation in bulding blocks

--- src/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ src/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -1,9 +1,9 @@
-﻿namespace Catalog.API.Exceptions;
+﻿
+namespace Catalog.API.Exceptions;
 
-public class ProductNotFoundException : Exception
+public class ProductNotFoundException : NotFoundException
 {
-    public ProductNotFoundException()
-        : base("Product not found!")
+    public ProductNotFoundException(Guid Id) : base("Product", Id)
     {
 
     }
--- src/Catalog/Catalog.API/Program.cs
+++ src/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,3 @@
-using BuildingBlocks.Behaviors;
-
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -20,7 +18,10 @@ builder.Services.AddMarten(config =>
     config.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 app.Run();
