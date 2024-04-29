5da98b8ec794251ccd69f6271f9287db0e2cb2d0
lucasrcbranco <lucasrcbranco@gmail.com>
2024-04-29 19:57:38 -0300

    Adding Configuration files for all models and configuring connection string for database

---
+++ src/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace OrderIteming.Infrastructure.Data.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(oi => oi.Id);
+
+        builder
+            .Property(oi => oi.Id)
+            .HasConversion(OrderItemId => OrderItemId.Value, dbId => OrderItemId.Of(dbId));
+
+        builder
+            .HasOne<Product>()
+            .WithMany()
+            .HasForeignKey(oi => oi.ProductId);
+
+        builder
+            .HasOne<Order>()
+            .WithMany(o => o.OrderItems)
+            .HasForeignKey(oi => oi.OrderId);
+
+        builder.Property(oi => oi.Quantity).IsRequired();
+        builder.Property(oi => oi.Price).IsRequired();
+    }
+}
---
+++ src/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
