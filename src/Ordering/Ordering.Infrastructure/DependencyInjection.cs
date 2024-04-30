683c0f267a06a4bcf779c849226398399900032b
lucasrcbranco <lucasrcbranco@gmail.com>
2024-04-29 21:02:43 -0300

    Adding DispatchDomainEvents interceptor

--- src/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,20 +1,23 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(
-        this IServiceCollection services, 
+        this IServiceCollection services,
         IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        services.AddDbContext<ApplicationDbContext>(opt =>
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, opt) =>
         {
-            opt.AddInterceptors(new AuditableEntityInterceptor());
+            opt.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             opt.UseSqlServer(connectionString);
         });
         return services;
--- src/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -5,3 +5,4 @@ global using Ordering.Domain.Enums;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Infrastructure.Data;
+global using Ordering.Infrastructure.Data.Interceptors;
