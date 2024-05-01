d78013a8c64fdfe0f7797b7ba3f103d5033c630c
lucasrcbranco <lucasrcbranco@gmail.com>
2024-05-01 16:57:53 -0300

    Creating application folder structure and dtos

---
+++ src/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Application.Dtos;
+
+public sealed record PaymentDto(
+    string CardName,
+    string CardNumber,
+    string Expiration,
+    string Cvv,
+    int PaymentMethod);
