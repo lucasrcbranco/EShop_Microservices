d58d924f0746da863d391f1eab9869293541646a
lucasrcbranco <lucasrcbranco@gmail.com>
2024-05-01 17:27:24 -0300

    Creating DeleteOrder funcionality and adding logging and validation behaviors to mediatR in application layer

---
+++ src/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public sealed record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+
+public sealed record DeleteOrderResult(bool IsSuccess);
+
+public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderCommandValidator()
+    {
+        RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required");
+    }
+}
