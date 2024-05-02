713fd2ffdd8d8d1b1bfa3a15b62654e7f7ed7384
lucasrcbranco <lucasrcbranco@gmail.com>
2024-05-01 21:11:01 -0300

    Adding all endpoints in ordering api layer

---
+++ src/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,25 @@
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints;
+
+public sealed record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{orderId}", async (Guid orderId, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(orderId));
+            var response = result.Adapt<DeleteOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithName("DeleteOrder")
+        .WithDescription("Deletes an existing Order")
+        .WithSummary("Deletes an existing Order");
+    }
+}
