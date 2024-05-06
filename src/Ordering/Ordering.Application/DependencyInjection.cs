c9ff46f1dde918b90f108b8550f117ac47f46761
lucasrcbranco <lucasrcbranco@gmail.com>
2024-05-06 20:43:45 -0300

    Adding feature manager into bulding blocks and using it to condionally activate OrderCreatedEvent

--- src/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Ordering/Ordering.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using BuildingBlocks.Messaging.MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 using System.Reflection;
 
 namespace Ordering.Application;
@@ -18,6 +19,8 @@ public static class DependencyInjection
         });
 
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddFeatureManagement();
 
         services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
--- src/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,16 +1,20 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 
-public class OrderCreatedEventHandler(IPublishEndpoint publisher, ILogger<OrderCreatedEventHandler> logger)
+public class OrderCreatedEventHandler(IPublishEndpoint publisher, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+        if (await featureManager.IsEnabledAsync("FeatureManagement"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
 
-        await publisher.Publish(orderCreatedIntegrationEvent, cancellationToken);
+            await publisher.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
     }
 }
