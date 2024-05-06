9e439be6d210849edce47f73e6946e0dacb99635
lucasrcbranco <lucasrcbranco@gmail.com>
2024-05-06 20:40:28 -0300

    Updating OrderCreatedEventHander to dispatch IntegrationEvent after order created, in order to consume later as order fufillement event

--- src/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
+++ src/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -37,4 +37,36 @@ public static class OrderExtensions
                      oi.Quantity,
                      oi.Price)).ToList()));
     }
+
+    public static OrderDto ToOrderDto(this Order order)
+    {
+        return new OrderDto(
+            order.Id.Value,
+            order.CustomerId.Value,
+            order.OrderName.Value,
+            ShippingAddress: new AddressDto(
+                order.ShippingAddress.FirstName,
+                order.ShippingAddress.LastName,
+                order.ShippingAddress.EmailAddress,
+                order.ShippingAddress.AddressLine,
+                order.ShippingAddress.Country,
+                order.ShippingAddress.State,
+                order.ShippingAddress.ZipCode),
+            BillingAddress: new AddressDto(
+                order.BillingAddress.FirstName,
+                order.BillingAddress.LastName,
+                order.BillingAddress.EmailAddress,
+                order.BillingAddress.AddressLine,
+                order.BillingAddress.Country,
+                order.BillingAddress.State,
+                order.BillingAddress.ZipCode),
+            Payment: new PaymentDto(
+                order.Payment.CardName,
+                order.Payment.CardNumber,
+                order.Payment.Expiration,
+                order.Payment.CVV,
+                order.Payment.PaymentMethod),
+            order.Status,
+            OrderItems: order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList());
+    }
 }
--- src/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,11 +1,16 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
+﻿using MassTransit;
 
-public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler(IPublishEndpoint publisher, ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
-    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
-        return Task.CompletedTask;
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+
+        await publisher.Publish(orderCreatedIntegrationEvent, cancellationToken);
     }
 }
